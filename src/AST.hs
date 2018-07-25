b56f6a6738250a5ed5641084f89ba77c5e66106e
handora <qcdsr970209@gmail.com>
2018-07-25 08:30:26 -0500

    add AST

--- src/AST.hs
+++ src/AST.hs
@@ -1,6 +1,14 @@
 module AST where
 
+-- import Parser 
+
 class Parse2AST a where
   convert :: a -> AST
 
-data AST = A
+data AST = AST [ImportNode] [FieldNode] [MethodNode]
+         deriving (Show)
+
+data ImportNode = ImportNode AId derving (Show)
+
+
+
